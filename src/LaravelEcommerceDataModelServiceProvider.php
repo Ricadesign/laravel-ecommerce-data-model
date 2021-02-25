b6760b0839a62e415138032b26bafe3098043d98
David <dluque@rica.design>
2021-02-25 13:40:02 +0100

    Publish seeders and Nova resources

--- src/LaravelEcommerceDataModelServiceProvider.php
+++ src/LaravelEcommerceDataModelServiceProvider.php
@@ -25,7 +25,9 @@ class LaravelEcommerceDataModelServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/database/migrations' => database_path('migrations'),
             __DIR__.'/database/factories' => database_path('factories'),
+            __DIR__.'/database/seeders' => database_path('seeders'),
             __DIR__.'/models' => app_path('Models'),
-        ]);
+            __DIR__.'/resources' => app_path('Nova'),
+        ], 'ecommerce-data-model');
     }
 }
