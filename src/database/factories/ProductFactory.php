ca93b499e54f63953531002c6a69dac0dfb7e131
David <dluque@rica.design>
2021-02-25 11:23:44 +0100

    Add factories

---
+++ src/database/factories/ProductFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Product::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $name = $this->faker->name,
+            'slug' =>  Str::slug($name),
+            'short_description' => $this->faker->sentence(3, false),
+            'description' => $this->faker->paragraph(3, false),
+            'price' => $this->faker->randomFloat(2, 10, 100),
+            'stock' => 100,
+            'visible' => true
+        ];
+    }
+}
