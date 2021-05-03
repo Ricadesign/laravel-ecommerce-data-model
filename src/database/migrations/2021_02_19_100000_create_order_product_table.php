bb1185bc027a45ad13372bd3d202db8da45799b8
David <dluque@rica.design>
2021-05-03 13:45:01 +0200

    Remove composite key in migration

--- src/database/migrations/2021_02_19_100000_create_order_product_table.php
+++ src/database/migrations/2021_02_19_100000_create_order_product_table.php
@@ -14,14 +14,13 @@ class CreateOrderProductTable extends Migration
     public function up()
     {
         Schema::create('order_product', function (Blueprint $table) {
+            $table->id();
             $table->foreignId('order_id')->constrained()->onDelete('cascade');
             $table->foreignId('product_id')->constrained();
             $table->string('product_name');
             $table->unsignedBigInteger('quantity');
             $table->decimal('price');
             $table->timestamps();
-
-            $table->primary(['order_id', 'product_id']);
         });
     }
 
